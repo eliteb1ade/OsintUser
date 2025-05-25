729df332f5d438ab31666368605554a2ca29f4fc
eliteb1ade <abdullahajan1976@gmail.com>
2025-05-25 18:45:27 +0500

    Prepare Flask app for Render deployment

--- src/api.py
+++ src/api.py
@@ -2,6 +2,13 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 import subprocess
 import json
+import os
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
 
 app = Flask(__name__)
 CORS(app)
