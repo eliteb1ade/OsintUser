5dc1d53ca2cd8fb0e7748c75ba93fa1a80c27504
eliteb1ade <abdullahajan1976@gmail.com>
2025-05-26 08:45:09 +0500

    added another endpoint

--- backend/api.py
+++ backend/api.py
@@ -52,3 +52,7 @@ def search_username():
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
+@app.route('/')
+def home():
+    return jsonify({'message': 'API is running! Use /search?username=...'})
